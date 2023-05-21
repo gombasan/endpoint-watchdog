c144500da901d79d4958a8432ef5cfe81e4931a7
hantaehee <xogmlwkdi@naver.com>
2023-05-21 16:20:29 +0900

    feat(Entity) : BaseEntity 추가 - 추가된 날짜, 업데이트 날짜를 공통으로 사용하기떄문에 BaseEntity 클래스를 추가하였습니다.

---
+++ src/main/java/com/clover/monitor/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.clover.monitor.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_date", updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	@Column(name = "updated_date")
+	private LocalDateTime updatedDate;
+
+}
